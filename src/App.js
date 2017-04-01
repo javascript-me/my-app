d084097a912cd69c3977d19eb4d77bbe3f8b2c3c
agileabc <agileabc@sina.com>
2017-04-02 00:13:12 +0800

    Remove useless code.

--- src/App.js
+++ src/App.js
@@ -9,11 +9,8 @@ export default class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to TW! </h2>
         </div>
-        <p className="App-intro">
-          Welcome to TW!
-        </p>
 
         <Agents />
 
