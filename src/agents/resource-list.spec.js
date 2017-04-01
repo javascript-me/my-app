c7d0408b343bc176b16d4376837023c4fed7ad55
agileabc <agileabc@sina.com>
2017-04-01 23:25:32 +0800

    Add unit test.

--- src/agents/resource-list.spec.js
+++ src/agents/resource-list.spec.js
@@ -6,15 +6,21 @@ import ResourceList from './resource-list'
 describe('ResourceList', () => {
 
     it('render() should be working fine', () => {
-        assert.equal(1, 1)
+
         let resources = [
             'ubuntu',
             'firefox3',
             'core-duo'
         ]
-        let component = mount(<ResourceList resources={resources} />)
-        assert.equal(1, component.find('.resource-list').length)
 
+        let component = mount(<ResourceList resources={resources} onDelete={(item) => {
+            assert.equal('ubuntu', item)
+        }} />)
+
+        assert.equal(1, component.find('.resource-list').length)
+        assert.equal(3, component.find('.delete-icon').length)
+
+        component.find('.delete-icon').at(0).simulate('click')
     })
 
 })
