a097b0965ce191c93fab72968141a3672dc5b94c
agileabc <agileabc@sina.com>
2017-04-11 01:33:33 +0800

    Working solution.

--- src/agents/resource-adding-panel.js
+++ src/agents/resource-adding-panel.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
+
+
 
 export default class ResourceAddingPanel extends React.Component {
 
@@ -13,6 +16,39 @@ export default class ResourceAddingPanel extends React.Component {
         }
     }
 
+    static containsChild (host, target) {
+        if (!host) return false
+        if (!target) return false
+        if (host === target) return true
+        return ResourceAddingPanel.containsChild(host, target.parentNode)
+    }
+
+    componentDidMount () {
+        let thisReactDOM = ReactDOM
+
+        window.addEventListener('click', (e) => {
+            let clickedNode = e.target
+            let resourceAddingPanelNode = thisReactDOM.findDOMNode(this.refs.resourceCreator);
+            //let node = ReactDOM.findDOMNode(this.refs.resourceCreator)
+            // console.log(node)
+            console.log(clickedNode)
+            console.log(resourceAddingPanelNode)
+
+            if (clickedNode === resourceAddingPanelNode) {
+                console.log('same node')
+            }
+
+            if (clickedNode.parentNode === resourceAddingPanelNode) {
+                console.log('is direct child node')
+            }
+
+            if (ResourceAddingPanel.containsChild(resourceAddingPanelNode, clickedNode)) {
+                console.log('containsChild...')
+            }
+
+        })
+    }
+
     changeText (e) {
         this.setState({
             value: e.target.value
@@ -24,7 +60,7 @@ export default class ResourceAddingPanel extends React.Component {
     }
 
     render () {
-        return <div className='resource-creator'>
+        return <div ref='resourceCreator' className='resource-creator'>
             <span className="icon-triangle"></span>
             <div>(Separate multiple resources name with commas)</div>
             <input className='input-field' type='text' value={this.state.value} onChange={this.changeText}></input>
