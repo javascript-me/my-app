2c42249ea18e10a05eb0c8a2e4eb5faba640c819
agileabc <agileabc@sina.com>
2017-04-11 02:13:10 +0800

    Change little css.

--- src/css-patterns/center-div/center-div.js
+++ src/css-patterns/center-div/center-div.js
@@ -4,7 +4,7 @@ export default class CssPatterns extends React.Component {
 
     render () {
         return <div className="outter-container">
-            <div className="inner-container">Me</div>
+            <div className="inner-container">MM</div>
         </div>
     }
 }
