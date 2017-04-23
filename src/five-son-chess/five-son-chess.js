415b57f73093cacda2672eda9c5a1975d8d409d0
agileabc <agileabc@sina.com>
2017-04-23 20:41:55 +0800

    Add slim buttons.

--- src/five-son-chess/five-son-chess.js
+++ src/five-son-chess/five-son-chess.js
@@ -15,13 +15,29 @@ export default class FiveSonChess extends React.Component {
         this.reset = this.reset.bind(this)
 
         this.handleClick = this.handleClick.bind(this)
+        this.switchCanvasVersion = this.switchCanvasVersion.bind(this)
+        this.switchDomVersion = this.switchDomVersion.bind(this)
 
         this.state = {
             startSide: CONST.PIECE_COLOR_BLACK,
             sequence: [],
             stackSequence: [],
-            isWin: false
+            isWin: false,
+            showCanvasVersion: true,
+            showDomVersion: false
         }
+    }
+
+    switchCanvasVersion () {
+        this.setState({
+            showCanvasVersion: !this.state.showCanvasVersion
+        })
+    }
+
+    switchDomVersion () {
+        this.setState({
+            showDomVersion: !this.state.showDomVersion
+        })
     }
 
     handleClick (netX, netY) {
@@ -85,9 +101,16 @@ export default class FiveSonChess extends React.Component {
         return (
             <div className='five-sun-chess-canvas'>
                 <div className='board'>
-                    <CanvasChess sequence={this.state.sequence} onClick={this.handleClick} />
+                    {
+                        this.state.showCanvasVersion
+                        && <CanvasChess sequence={this.state.sequence} onClick={this.handleClick} />
+                    }
                 </div>
                 <div className='board'>
+                    <div className='button-group-slim'>
+                        <div className='button-slim' onClick={this.switchCanvasVersion}>{'<'}</div>
+                        <div className='button-slim' onClick={this.switchDomVersion}>{'>'}</div>
+                    </div>
                     <div className={this.getButtonClassNames(this.state.sequence)} onClick={this.undo}>悔棋</div>
                     <div className={this.getButtonClassNames(this.state.stackSequence)} onClick={this.redo}>进棋</div>
                     <div className='button' onClick={this.reset}>重置</div>
@@ -96,7 +119,10 @@ export default class FiveSonChess extends React.Component {
                     }
                 </div>
                 <div className='board'>
-                    <DomChess sequence={this.state.sequence} onClick={this.handleClick}  />
+                    {
+                        this.state.showDomVersion
+                        && <DomChess sequence={this.state.sequence} onClick={this.handleClick}  />
+                    }
                 </div>
             </div>
         )
